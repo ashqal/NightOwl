50252e69b0a8c1e174d73f07a750c329338d44ec
asha <ashnb@qq.com>
2015-11-13 11:38:18 +0800

    add listview support

---
+++ nightowllib/src/main/java/com/asha/nightowllib/handler/impls/ListViewHandler.java
@@ -0,0 +1,14 @@
+package com.asha.nightowllib.handler.impls;
+
+import android.widget.ListView;
+
+import com.asha.nightowllib.NightOwlTable;
+import com.asha.nightowllib.handler.annotations.OwlHandle;
+
+/**
+ * Created by hzqiujiadi on 15/11/13.
+ * hzqiujiadi ashqalcn@gmail.com
+ */
+@OwlHandle(ListView.class)
+public class ListViewHandler extends AbsSkinHandler implements NightOwlTable.OwlListView {
+}
